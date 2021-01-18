b5219ccb4048cbd4cc81d4e4bf30661e9188a595
Nikhil Bansal <nikhil.bansal@sirionlabs.com>
2021-01-18 23:21:17 +0530

    commit to nikhil

--- linear.py
+++ linear.py
@@ -27,14 +27,14 @@ y=dataset.iloc[:,-1]
 
 from sklearn.linear_model import LinearRegression
 
-linearReg = LinearRegression()
+linearReg1 = LinearRegression()
 # cross_val_score is actually used to do cross validation
 
 from sklearn.model_selection import cross_val_score
 
 # calculate mean squared error
 
-mse=cross_val_score(linearReg,x,y,scoring='neg_mean_squared_error',cv=5)
+mse=cross_val_score(linearReg1,x,y,scoring='neg_mean_squared_error',cv=5)
 
 mean_mse = np.mean(mse)
 
@@ -45,9 +45,9 @@ mean_mse = np.mean(mse)
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=.8,random_state=0)
 
-linearReg.fit(x_train,y_train)
+linearReg1.fit(x_train,y_train)
 
-y_predict_linear = linearReg.predict(x_test)
+y_predict_linear = linearReg1.predict(x_test)
 
 # plot
 sns.distplot(y_test-y_predict_linear)
